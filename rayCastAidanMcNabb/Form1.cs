de6155403759dd64b21481d67d74444e62d464b1
amcnabb67@gmail.com <amcnabb67@gmail.com>
2020-07-28 11:14:43 -0700

    two lines kinda work

--- rayCastAidanMcNabb/Form1.cs
+++ rayCastAidanMcNabb/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,31 +16,74 @@ namespace rayCastAidanMcNabb
         Graphics gfx;
 
         Bitmap canvas;
+        Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
         }
-        Point basePoint;
+        Point basePoint => this.PointToClient(Cursor.Position);
         int accuracy = 100;
-      
-        //start at top
+        (Point first, Point second) barrier;
 
+
+        //start at top
+       
         private void Form1_Load(object sender, EventArgs e)
         {
             canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             gfx = Graphics.FromImage(canvas);
+            barrier = generateBarrier();
+           
         }
+        double u(Point wall)
+        {
+     
+            double parallel = ((barrier.first.X - barrier.second.X) *
+                (basePoint.Y - wall.Y) -
+                (barrier.first.Y - barrier.second.Y) *
+                (basePoint.X - wall.X));
+
+            if (parallel == 0)
+            {
+                return parallel;
+            }
+            
+             return (barrier.first.X - basePoint.X) *
+             (basePoint.Y - wall.Y) -
+             (barrier.first.Y - basePoint.Y) *
+              (basePoint.X - wall.X) / parallel;            
+                
+               
+        }
+
+
         void generateRay(double angle)
         {
           
            double x = basePoint.X;
            double y = basePoint.Y;
+           
            while (x > 0 && y > 0 && x < ClientSize.Width && y < ClientSize.Height)
            {
                 x +=  Math.Cos(angle);
                 y +=  Math.Sin(angle);
            }
-           gfx.DrawLine(Pens.White, basePoint, new Point((int)x,(int)y));
+            Point bs = new Point((int)x, (int)y);
+            if (u(bs) >= 0 && u(bs) <= 1)
+            {
+                gfx.DrawLine(Pens.Blue, basePoint, bs);
+            }
+            else
+            {
+                gfx.DrawLine(Pens.White, basePoint, bs);
+            }
+
+        }
+        (Point, Point) generateBarrier()
+        {
+            return (new Point(100, 100), new Point(100, 200));
+            //return (new Point(rand.Next(0, ClientSize.Width), rand.Next(0, ClientSize.Height)),
+            //      new Point(rand.Next(0, ClientSize.Width), rand.Next(0, ClientSize.Height)));
         }
 
         private void Form1_Shown(object sender, EventArgs e)
@@ -50,11 +94,11 @@ namespace rayCastAidanMcNabb
         private void timer1_Tick(object sender, EventArgs e)
         {
             gfx.Clear(Color.Black);
-            Point cursor = this.PointToClient(Cursor.Position);
-            basePoint = cursor;//new Point(.X, e.Y);
+            gfx.DrawLine(Pens.Red, barrier.first, barrier.second);
+            //Point cursor = this.PointToClient(Cursor.Position);
+            //basePoint = cursor;
             for (int i = 0; i < accuracy; i++)
             {
-                //gfx.DrawLine(Pens.Black, basePoint, new Point(basePoint.X + (int)(700 * Math.Cos(2 * Math.PI * i / accuracy)), basePoint.Y + (int)(700 * Math.Sin(2 * Math.PI * i / accuracy))));
                 generateRay(2 * Math.PI * i / accuracy);
             }
 
